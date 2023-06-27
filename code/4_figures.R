a032336520a9df1ced28095db974dda8a97ea694
Urtzi_EU <urtzi.enriquez@gmail.com>
2023-06-27 10:39:31 +0200

    new figure versions

--- code/4_figures.R
+++ code/4_figures.R
@@ -3,80 +3,132 @@ setwd('./code')
 rm(list=ls())
 
 library(ggplot2)
-library(viridis)
-library(forcats)
+library(cowplot)
 library(tidyverse)
 source('./aux_functions.R')
-
 load('../results/micro_Jihlava.Rda')
 
-ecto_df_current_sun <- load_ectos(path = "../results",scenario = "cur", movement = "200cm", 
-                                  shading = "sun", micro = micro)
+# depth plots
 
-ecto_df_current_sun_summer <- subset_env(ecto_df_current_sun, season = "summer")
-ecto_df_current_sun_winter <- subset_env(ecto_df_current_sun, season = "winter")
+list_ec_cur_sun_200cm_winter <- list(ec_cur_sun_200cm_winter)
+list_ec_cur_sun_50cm_winter <- list(ec_cur_sun_50cm_winter)
+list_ec_cur_sun_200cm_summer <- list(ec_cur_sun_200cm_summer)
+list_ec_cur_sun_50cm_summer <- list(ec_cur_sun_50cm_summer)
+list_cc_200cm_winter <- list()
+list_cc_50cm_winter <- list()
+list_cc_200cm_summer <- list()
+list_cc_50cm_summer <- list()
+scen <- c('cc45', 'cc85', 'gf45', 'gf85', 'hg45', 'hg85')
+for(s in scen){
+  ec_cc_sun_200cm <- load_ectos(path = "../results",
+                                scenario = s,
+                                movement = "200cm",
+                                shading = "sun",
+                                micro = micro)
+  ec_cc_sun_50cm <- load_ectos(path = "../results",
+                               scenario = s,
+                               movement = "50cm",
+                               shading = "sun",
+                               micro = micro)
+  ec_cc_sun_200cm$model <- as.factor(substr(ec_cc_sun_200cm$model, 1, nchar(as.character(ec_cc_sun_200cm$model))-5)) # set the same name to plot them stacked
+  ec_cc_sun_50cm$model <- as.factor(substr(ec_cc_sun_50cm$model, 1, nchar(as.character(ec_cc_sun_50cm$model))-5)) # set the same name to plot them stacked
+  ec_cc_sun_200cm_summer <- subset_env(ec_cc_sun_200cm, season = "summer")
+  ec_cc_sun_200cm_winter <- subset_env(ec_cc_sun_200cm, season = "winter")
+  ec_cc_sun_50cm_summer <- subset_env(ec_cc_sun_50cm, season = "summer")
+  ec_cc_sun_50cm_winter <- subset_env(ec_cc_sun_50cm, season = "winter")
+  list_cc_200cm_summer[[s]] <- ec_cc_sun_200cm_summer
+  list_cc_200cm_winter[[s]] <- ec_cc_sun_200cm_winter
+  list_cc_50cm_summer[[s]] <- ec_cc_sun_50cm_summer
+  list_cc_50cm_winter[[s]] <- ec_cc_sun_50cm_winter
+}
+
+depth_p1 <- plot_depth(list_ec_cur_sun_200cm_winter, title='Current climate', subtitle='Winter - Movement up to 200 cm', labels='no')
+depth_p2 <- plot_depth(list_ec_cur_sun_50cm_winter, subtitle='Winter - 50 cm', labels='no', ylab='')
+depth_p3 <- plot_depth(list_ec_cur_sun_200cm_summer, subtitle='Summer - 200 cm', labels='no', ylab='')
+depth_p4 <- plot_depth(list_ec_cur_sun_50cm_summer, subtitle='Summer - 50 cm', labels='no', ylab='')
+depth_p5 <- plot_depth(list_cc_200cm_winter, title='2070', subtitle='Winter - Movement up to 200 cm', labels='yes')
+depth_p6 <- plot_depth(list_cc_50cm_winter, subtitle='Winter - 50 cm', labels='yes', ylab='')
+depth_p7 <- plot_depth(list_cc_200cm_summer, subtitle='Summer - 200 cm', labels='yes', ylab='')
+depth_p8 <- plot_depth(list_cc_50cm_summer, subtitle='Summer - 50 cm', labels='yes', ylab='')
+
+plot_all_depths <- plot_grid(depth_p1, depth_p2, depth_p3, depth_p4, 
+                             depth_p5, depth_p6, depth_p7, depth_p8,
+                             labels=c("a", "b", "c", "d", "e", "f", "g", "h"),
+                             label_size = 18, ncol = 4, nrow = 2, align='hv')
+
+pdf('../figures/depths.pdf', width = 18, height = 8)
+plot_all_depths
+dev.off()
 
 
+# Differences in cumulative oxygen consumption under current conditions 
+# given different behaviors and the presence/absence of seasonal acclimation
+# function ´plot_smr()´
 
-data <- ecto_df_current_sun
-csdf <- data %>%
-  group_by(model) %>%
-  dplyr::mutate(cs = cumsum(SMR))
+ec_cur_sun_200cm <- load_ectos(path = "../results",
+                               scenario = "current",
+                               movement = "200cm",
+                               shading = "sun",
+                               micro = micro)
+ec_cur_sun_50cm <- load_ectos(path = "../results",
+                              scenario = "current",
+                              movement = "50cm",
+                              shading = "sun",
+                              micro = micro)
 
-csdf %>% 
-  ggplot(aes(dates, cs, color = model)) +
-  geom_line(size = 1.5, alpha = .7) +
-  theme_minimal() +
-  #scale_x_continuous(expand = c(0, 0)) +
-  #scale_y_continuous(sec.axis = sec_axis(~ ., breaks = d_ends)) +
-  ggtitle("Orange trees getting bigger with age",
-          subtitle = "Based on the Orange data set in R") +
-  labs(x = "Days old", y = "Circumference (mm)", caption = "Plot by @drsimonj")
-
-
-
+ec_cur_sun_200cm_summer <- subset_env(ec_cur_sun_200cm, season = "summer")
+ec_cur_sun_200cm_winter <- subset_env(ec_cur_sun_200cm, season = "winter")
+ec_cur_sun_50cm_summer <- subset_env(ec_cur_sun_50cm, season = "summer")
+ec_cur_sun_50cm_winter <- subset_env(ec_cur_sun_50cm, season = "winter")
 
 
+p1 <- plot_smr(ec_cur_sun_200cm, period='all', title="Whole year")
+p2 <- plot_smr(ec_cur_sun_200cm_summer, period='subset', title="Summer", ylab='')
+p3 <- plot_smr(ec_cur_sun_200cm_winter, period='subset', title="Winter", ylab='')
+p4 <- plot_smr(ec_cur_sun_50cm, period='all', xlab='year of simulation')
+p5 <- plot_smr(ec_cur_sun_50cm_summer, period='subset', ylab='', xlab='hour of simulation')
+p6 <- plot_smr(ec_cur_sun_50cm_winter, period='subset', ylab='', xlab='hour of simulation')
 
-# # example Te traces
-# # pdf("./results/FigSX_Te_traces_unconstrained.pdf", height=20, width=10)
-# 
-# # par(mfrow=c(6,1), mar=c(3,4,0.5,0.5))
-# 
-# for(i in levels(ecto_df_current_sun$model)){
-#   
-#   environ <- ecto_df_current_sun[ecto_df_current_sun$model == i,]
-#   metout <- data.frame(micro$metout)
-#   
-#   # append dates
-#   days <- rep(seq(1, length(unique(environ$DAY))), 24)
-#   days <- days[order(days)]
-#   dates <- days+metout$TIME/60/24-1 # dates for hourly output
-#   
-#   
-#   with(environ, plot(TC ~ dates, ylab = "", xlab="", col = 'black', ylim = c(-70, 38), type = "l", yaxt = 'n'))
-#   with(environ, points(ACT * 2 - 10 ~ dates, type = "l", pch = 16, col = "orange"))
-#   with(environ, points(DEP/4 - 15 ~ dates, type = "l", col = "brown"))
-#   abline(4, 0, lty = 2, col = 'blue') # T_F_min
-#   abline(20, 0, lty = 2, col = 'red') # T_F_max
-#   abline(-2, 0, col = 'blue') # CT_min
-#   abline(36, 0, col = 'red') # CT_max
-#   ytick<-seq(0, 40, by=5)
-#   axis(side=2, at=ytick, labels = TRUE, las = 2, cex.axis = .7)
-#   mtext(text = c('Active', 'Inactive'), side = 2, line = 1, at = c(-6, -10), cex = .7, las = 2)
-#   ytick<-seq(-6, -10, by=-4)
-#   axis(side=2, at=ytick, labels = FALSE)
-#   mtext(text = rev(seq(0, 200, 40)), side = 2, line = 1, at = seq(-65, -15, length.out=6), las = 2, cex = .7)
-#   ytick<-seq(-65, -15, length.out=6)
-#   axis(side=2, at=ytick, labels = FALSE)
-#   abline(h = -15, lty = 2, col = 'grey')
-#   mtext(text = c('body temperature (°C)', 'depth (cm)'), side = 2, line = 2.5, at = c(22, -40), cex = .7)
-#   text(15, c(20 + 1, 4 + 1), c('VTmax', 'VTmin'), col = c('red', 'blue'), cex = 0.75)
-#   text(15, c(36 + 1, -2 + 1), c('CTmax', 'CTmin'), col = c('red', 'blue'), cex = 0.75)
-#   
-# }
-# 
-# 
-# # dev.off()
+
+plot_all_smr <- plot_grid(p1, p2, p3, p4, p5, p6,
+                          labels=c("a", "b", "c", "d", "e", "f"),
+                          label_size = 18, ncol = 3, nrow = 2, align='h')
+
+pdf('../figures/smr_current.pdf', width = 13, height = 8)
+print(plot_all_smr)
+dev.off()
+
+
+# CLIMATE CHANGE scenarios: differences in cum oxygen consumption change (delta)
+# given different behaviors and the presence/absence of seasonal acclimation
+# function ´plot_delta_smr()´
+
+pdf('../figures/delta_smr.pdf', width = 13, height = 8)
+scen <- c('cc45', 'cc85', 'gf45', 'gf85', 'hg45', 'hg85')
+for(s in scen){
+  ec_cc_sun_200cm <- load_ectos(path = "../results",
+                                scenario = s,
+                                movement = "200cm",
+                                shading = "sun",
+                                micro = micro)
+  ec_cc_sun_50cm <- load_ectos(path = "../results",
+                               scenario = s,
+                               movement = "50cm",
+                               shading = "sun",
+                               micro = micro)
+  
+  delta_p1 <- plot_delta_smr(ec_cur_sun_200cm, ec_cc_sun_200cm, period='all', title=paste("Whole year", s, sep=' - '))
+  delta_p2 <- plot_delta_smr(ec_cur_sun_200cm, ec_cc_sun_200cm, period='summer', title="Summer", ylab='')
+  delta_p3 <- plot_delta_smr(ec_cur_sun_200cm, ec_cc_sun_200cm, period='winter', title="Winter", ylab='')
+  delta_p4 <- plot_delta_smr(ec_cur_sun_50cm, ec_cc_sun_50cm, period='all', xlab='year of simulation')
+  delta_p5 <- plot_delta_smr(ec_cur_sun_50cm, ec_cc_sun_50cm, period='summer', ylab='', xlab='hour of simulation')
+  delta_p6 <- plot_delta_smr(ec_cur_sun_50cm, ec_cc_sun_50cm, period='winter', ylab='', xlab='hour of simulation')
+  
+  plot_all_delta_smr <- plot_grid(delta_p1, delta_p2, delta_p3, delta_p4, delta_p5, delta_p6, 
+                                  labels=c("a", "b", "c", "d", "e", "f"),
+                                  label_size = 18, ncol = 3, nrow = 2, align='h')
+  print(plot_all_delta_smr)
+}
+dev.off()
 
 
--- code/aux_functions.R
+++ code/aux_functions.R
@@ -1,10 +1,13 @@
 
+#########
+# Auxiliary functions
+
+
 # Calculate RMSEs
 rmse.calc <- function(pred, obs, depth=1, micro){
   env_test <- pred
   env_test$datetime <- micro$dates
   env_test <- merge(env_test, obs, by="datetime")
-  
   round(sqrt(mean((env_test[,ncol(env_test)] - env_test[,3+depth])^2)), 3)
 }
 
@@ -19,25 +22,13 @@ smr.calc <- function(ecto){
 }
 
 # function to load ectotherm objects
-
-# TO DO
-
-#     scenario = "cur" # c("cur", "C", "G", "H")
-#     movement = "200cm" # c("200cm", "50cm")
-#     shading = "sun" # c("sun","shade")
-
-# and make it rearrange the data to have cols for models, constraints, 
-# acc/no acc (for that might need to rbind)
-# each ecto goes twice: once with smr and the other with acc_smr
-
 load_ectos <- function(path = path,
-                       scenario = c("cur", "C", "G", "H"), movement = c("200cm", "50cm"), 
-                       shading = c("sun","shade"), micro = micro){
-  if(scenario == "cur"){
-    patt <- paste0("*",movement,"_",shading,"_current.Rda")
-  }
+                       scenario = c("current", "cc45", "cc85", "gf45", "gf85", "hg45", "hg85"), 
+                       movement = c("200cm", "50cm"), 
+                       shading = c("sun","shade"), 
+                       micro = micro){
+  patt <- paste0("*", movement, "_", shading, "_", scenario)
   ecto_files <- list.files(path=path, pattern=patt, full.names=T)
-  
   a <- 1
   for(ec in ecto_files){
     load(ec)
@@ -71,6 +62,122 @@ subset_env <- function(ectos_df, season = c("summer", "winter")){
 }
 
 
-# some plotting function
+#########
+# plotting functions
+
+# plot cumulative O2 consumption (~cumulative smr)
+plot_smr <- function(data, period=c('all', 'subset'), title='', 
+                     xlab = "", ylab = expression(paste('cumulative ', O[2], ' consumed (ml)'))){
+  csdf <- data %>%
+    dplyr::group_by(model) %>%
+    dplyr::mutate(cs = cumsum(SMR)) %>%
+    dplyr::mutate(cs_acc = cumsum(SMR_acc))
+  
+  if(period == 'subset'){
+    csdf$hour <- NA
+    for(i in unique(csdf$model)){
+      csdf$hour[csdf$model == i] <- 1:length(csdf$hour[csdf$model == i])
+    }
+    csdf %>% 
+      ggplot() +
+      geom_line(aes(hour, cs, color = model), linetype = "solid", linewidth = 0.7) +
+      geom_line(aes(hour, cs_acc, color = model), linetype = "dashed", linewidth = 0.7) + 
+      scale_colour_manual(values=c('#0059FF', '#86B0FF', '#FF8300', '#FEBB74', '#01BB04', '#8AFF86')) +
+      theme_minimal() +
+      theme(legend.position = 'none') +
+      ggtitle(title) +
+      labs(x = xlab, y = ylab)
+  } else {
+    csdf %>% 
+      ggplot() +
+      geom_line(aes(dates, cs, color = model), linetype = "solid", linewidth = 0.7) +
+      geom_line(aes(dates, cs_acc, color = model), linetype = "dashed", linewidth = 0.7) + 
+      scale_colour_manual(values=c('#0059FF', '#86B0FF', '#FF8300', '#FEBB74', '#01BB04', '#8AFF86'),
+                          labels=c('cold', 'cold-moist', 'warm', 'warm-moist', 'passive', 'passive-moist')) +
+      theme_minimal() +
+      theme(legend.position = c(0.2, 0.70)) +
+      ggtitle(title) +
+      labs(x = xlab, y = ylab)
+  }
+}
 
 
+# plot cumulative O2 consumption increase with CC scenario
+plot_delta_smr <- function(data_baseline, data_cc,
+                           period=c('all', 'summer', 'winter'), title='', 
+                           xlab = "", ylab = expression(paste('cumulative increase in ', O[2], ' consumed (ml)'))){
+  data_cc$deltaSMR <- data_cc$SMR - data_baseline$SMR
+  data_cc$deltaSMR_acc <- data_cc$SMR_acc - data_baseline$SMR_acc
+  if(period == 'summer'){
+    data_cc <- subset_env(data_cc, season = "summer")
+  } else if(period == 'winter'){
+    data_cc <- subset_env(data_cc, season = "winter")
+  }
+  csdf <- data_cc %>%
+    dplyr::group_by(model) %>%
+    dplyr::mutate(cs = cumsum(deltaSMR)) %>%
+    dplyr::mutate(cs_acc = cumsum(deltaSMR_acc))
+  
+  if(period != 'all'){
+    csdf$hour <- NA
+    for(i in unique(csdf$model)){
+      csdf$hour[csdf$model == i] <- 1:length(csdf$hour[csdf$model == i])
+    }
+    csdf %>% 
+      ggplot() +
+      geom_line(aes(hour, cs, color = model), linetype = "solid", linewidth = 0.7) +
+      geom_line(aes(hour, cs_acc, color = model), linetype = "dashed", linewidth = 0.7) + 
+      geom_hline(yintercept=0, linetype='dashed') +
+      scale_colour_manual(values=c('#0059FF', '#86B0FF', '#FF8300', '#FEBB74', '#01BB04', '#8AFF86')) +
+      theme_minimal() +
+      theme(legend.position = 'none') +
+      ggtitle(title) +
+      labs(x = xlab, y = ylab)
+  } else {
+    csdf %>% 
+      ggplot() +
+      geom_line(aes(dates, cs, color = model), linetype = "solid", linewidth = 0.7) +
+      geom_line(aes(dates, cs_acc, color = model), linetype = "dashed", linewidth = 0.7) + 
+      geom_hline(yintercept=0, linetype='dashed') +
+      scale_colour_manual(values=c('#0059FF', '#86B0FF', '#FF8300', '#FEBB74', '#01BB04', '#8AFF86'),
+                          labels=c('cold', 'cold-moist', 'warm', 'warm-moist', 'passive', 'passive-moist')) +
+      theme_minimal() +
+      theme(legend.position = c(0.2, 0.70)) +
+      ggtitle(title) +
+      labs(x = xlab, y = ylab)
+  }
+}
+
+
+# plot selected depths
+plot_depth <- function(data_list, title = '', subtitle = '', ylab='Depth (cm)', labels=c('yes', 'no')){ # data_list has to be a list length >= 1
+  p <- ggplot()
+  for(i in 1:length(data_list)){
+    if(length(data_list) == 1){
+      p <- p + geom_violin(data = data_list[[i]], aes(x = model, y = DEP, fill = model),
+                           scale='width', position='identity')
+    } else {
+      p <- p + geom_violin(data = data_list[[i]], aes(x = model, y = DEP, fill = model),
+                           scale='width', position='identity', alpha = 0.4)
+    }
+  }
+  if(labels == 'no'){
+    p +
+      scale_fill_manual(values=c('#0059FF', '#86B0FF', '#FF8300', '#FEBB74', '#01BB04', '#8AFF86'),
+                        labels=c('cold', 'cold-moist', 'warm', 'warm-moist', 'passive', 'passive-moist')) +
+      scale_x_discrete(labels = c('cold', 'cold-moist', 'warm', 'warm-moist', 'passive', 'passive-moist')) +
+      theme_classic() +
+      theme(legend.position = "none", axis.text.x = element_blank()) + 
+      ggtitle(title, subtitle = subtitle) +
+      labs(x = '', y = ylab)
+  } else if(labels == 'yes'){
+    p +
+      scale_fill_manual(values=c('#0059FF', '#86B0FF', '#FF8300', '#FEBB74', '#01BB04', '#8AFF86'),
+                        labels=c('cold', 'cold-moist', 'warm', 'warm-moist', 'passive', 'passive-moist')) +
+      scale_x_discrete(labels = c('cold', 'cold-moist', 'warm', 'warm-moist', 'passive', 'passive-moist')) +
+      theme_classic() +
+      theme(legend.position = "none", axis.text.x = element_text(angle = 45, hjust = 1)) + 
+      ggtitle(title, subtitle = subtitle) +
+      labs(x = '', y = ylab)
+  }
+}
